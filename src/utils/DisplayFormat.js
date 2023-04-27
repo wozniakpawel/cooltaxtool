ac4e0a906e032f23fd4787b7cdd3f188056c3287
Vijay Raghavendra Srinivas <vijayraghav22@gmail.com>
2023-04-28 01:14:24 +0530

    Comma-separate thousands and millions in the salary range slider #6 (#35)
    Closes #6 
    resolves Comma-separate thousands and millions in the salary range
    slider #6
    refactored function numberWithCommas(x) in TaxBreakdown to new utils so
    that it can be reused by other components

---
+++ src/utils/DisplayFormat.js
@@ -0,0 +1,8 @@
+export function numberWithCommas(x) {
+    return new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "GBP",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(x);
+}
