bc18f3809b32a07eef03063845521aea1ceebb76
Karam Agha <ak703@live.com>
2023-04-30 01:23:59 +0100

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { defaultInputs, UserMenu } from './components/UserMenu';
-import TaxBreakdown from './components/TaxBreakdown';
-import PensionAnalysis from './components/PensionAnalysis';
-import TaxYearOverview from './components/TaxYearOverview';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { defaultInputs, UserMenu } from "./components/UserMenu";
+import TaxBreakdown from "./components/TaxBreakdown";
+import PensionAnalysis from "./components/PensionAnalysis";
+import TaxYearOverview from "./components/TaxYearOverview";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [userInputs, setUserInputs] = React.useState(defaultInputs);
+  const [theme, setTheme] = React.useState("dark");
+
+  React.useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   const handleUserInputsChange = (inputs) => {
     setUserInputs(inputs);
   };
+
+  function toggleTheme() {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+
+  // takes plotly theme data as JSON and adds dark theme if necessary
+  function setPlotTheme(layout) {
+    if (theme === "dark") {
+      layout.paper_bgcolor = "#333";
+      layout.plot_bgcolor = "#222";
+      layout.font = { color: "#fff" };
+    }
+    return layout;
+  }
 
   return (
     <Container fluid className="d-flex flex-column min-vh-100 p-0">
       <Container className="page-content p-0">
         <Row>
           <Col>
-            <Header />
+            <Header theme={theme} themeToggleFunction={toggleTheme} />
             <UserMenu onUserInputsChange={handleUserInputsChange} />
-            {
-              userInputs.incomeAnalysis &&
-              <PensionAnalysis inputs={userInputs} />
-            }
+            {userInputs.incomeAnalysis && (
+              <PensionAnalysis inputs={userInputs} plotThemer={setPlotTheme} />
+            )}
           </Col>
           <Col>
-            {
-              userInputs.incomeAnalysis &&
-              <TaxBreakdown inputs={userInputs} />
-            }
-            {
-              !userInputs.incomeAnalysis &&
-              <TaxYearOverview inputs={userInputs} />
-            }
+            {userInputs.incomeAnalysis && (
+              <TaxBreakdown inputs={userInputs} theme={theme} />
+            )}
+            {!userInputs.incomeAnalysis && (
+              <TaxYearOverview inputs={userInputs} plotThemer={setPlotTheme} />
+            )}
           </Col>
           {/* <Col>
           </Col> */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Form } from "react-bootstrap";
 
-const Header = () => {
-    return (
-        <Navbar>
-            <Container className="justify-content-center large">
-                <Navbar.Brand>
-                    <h1>
-                        <strong>
-                            Cool<span className="text-danger">Tax</span>Tool
-                        </strong>
-                    </h1>
-                </Navbar.Brand>
-            </Container>
-        </Navbar>
-    );
+const Header = (props) => {
+  return (
+    <Navbar>
+      <Container className="justify-content-center large">
+        <Navbar.Brand>
+          <h1>
+            <strong>
+              Cool<span className="text-danger">Tax</span>Tool
+            </strong>
+            {/* <button onClick={props.themeToggleFunction}>Current theme:</button> */}
+            <Form.Check
+              type="switch"
+              id="themeToggle"
+              label="Dark mode"
+              name="themeToggle"
+              checked={props.theme === "dark"}
+              onChange={props.themeToggleFunction}
+            />
+          </h1>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
