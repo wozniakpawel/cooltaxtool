9b6ceb4bef35b052d857ccdc7fd7ed51efdb6432
Pawel Wozniak <rax1994@gmail.com>
2026-01-17 21:18:10 +0000

    Move from Rechart to Apex Charts

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock react-apexcharts for testing (ApexCharts requires browser APIs not available in Jest)
+jest.mock('react-apexcharts', () => {
+  return function MockChart() {
+    return <div data-testid="mock-chart" />;
+  };
+});
